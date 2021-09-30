af96010bd7fbc3d5818c9f682cb486cff5eee446
davifeliciano <dfeliciano37@gmail.com>
2021-09-30 18:54:40 -0300

    Adding a class for animations and a dir for images

--- ising.py
+++ ising.py
@@ -16,20 +16,10 @@ class Ising:
     ) -> None:
 
         self.lattice = Lattice(shape, temp, j, field, init_state)
-        self.fig, self.ax = plt.subplots()
-        self.animation = FuncAnimation(
-            self.fig, self.update_animation, interval=100, save_count=60
-        )
-
-        self.mean_energy_hist = []
-        self.magnet_hist = []
-        self.specific_heat_hist = []
-        self.susceptibility_hist = []
-
-    def update_animation(self, frame):
-        self.ax.clear()
-        self.ax.imshow(self.lattice.state, norm=Normalize(vmin=-1.0, vmax=1.0))
-        self.update()
+        self.mean_energy_hist = [self.lattice.mean_energy()]
+        self.magnet_hist = [self.lattice.magnet()]
+        self.specific_heat_hist = [self.lattice.specific_heat()]
+        self.susceptibility_hist = [self.lattice.susceptibility()]
 
     def update(self):
         self.mean_energy_hist.append(self.lattice.mean_energy())
@@ -39,6 +29,34 @@ class Ising:
         self.lattice.update()
 
 
+class AnimatedIsing(Ising):
+    def __init__(
+        self,
+        shape=(30, 30),
+        temp=2,
+        j=(1, 1),
+        field=0,
+        init_state="random",
+    ) -> None:
+
+        super().__init__(
+            shape=shape,
+            temp=temp,
+            j=j,
+            field=field,
+            init_state=init_state,
+        )
+        self.fig, self.ax = plt.subplots()
+        self.animation = FuncAnimation(
+            self.fig, self.update_animation, interval=100, save_count=60
+        )
+
+    def update_animation(self, frame):
+        self.ax.clear()
+        self.ax.imshow(self.lattice.state, norm=Normalize(vmin=-1.0, vmax=1.0))
+        self.update()
+
+
 if __name__ == "__main__":
-    ising = Ising(shape=(128, 128), temp=1.0, init_state="random")
-    ising.animation.save("ising.gif")
+    ising = AnimatedIsing(shape=(128, 128))
+    ising.animation.save("images/test.gif")
